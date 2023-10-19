a887b6cdd45d852c418cd55d3c0adf45c9dfdaa7
JesseniaMera <145022072+JesseniaMera@users.noreply.github.com>
2023-10-19 08:09:07 -0500

    Add files via upload

---
+++ cajeroautomatico.cpp
@@ -0,0 +1,28 @@
+//programa: cajero atomatico
+//autor:Jessenia Mera
+//fecha:12-10-2023
+//archivo:cajeroautomatico.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+	int c1, c2, c3, c01=22, c02=30, c03=30;
+	float valor, saldodisponible= 10.000;
+	cout<<"Ingrese c1 c2 c3:"; cin>>c1>>c2>>c3;
+	if(c1==c01){
+		if(c2==c02){
+			if(c3==c03){
+				cout<<"Ingrese el valor a retirar:";cin>>valor;
+				saldodisponible =saldodisponible-valor;
+			}else{
+				cout<<"contraseña c3 invalida"<<endl;
+			}
+		}else{
+			cout<<"contraseña c2 incorrecta"<<endl;
+		}
+	}else{
+		cout<<"contraseña c1 incorrecta"<<endl;
+	}return(0);
+}
+
+
---
+++ ingrseclaveMeraOlivoJess.cpp
@@ -0,0 +1,18 @@
+//programa:clave
+//autor:JeJessen Mera Olivo
+//fecha: 2023-10-05
+#include <iostream>
+using namespace std;
+int main()
+{
+	int c1,c2=5;
+		cout<<"Ingrese la clave"; cin>>c1;
+	if(c1==c2)
+	{
+		cout<<"Acceso correcto";
+	} else{
+		cout<<"Acceso incorrecto";
+	}
+	return(0);
+}
+
